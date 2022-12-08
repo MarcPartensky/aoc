af75e7d6e6261df2c48c702fe64b78d1bb23625f
Marc Partensky <marc.partensky@gmail.com>
2022-12-09 00:39:18 +0100

    the right answer

--- 2022/day1/__main__.py
+++ 2022/day1/__main__.py
@@ -1,21 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import sys
-
-elf = 0
-elf_i = 0
-max_elf = 0
-max_elf_i = 0
-
-for line in sys.stdin:
-    line = line.strip()
-    if len(line) == 0:
-        if elf > max_elf:
-            max_elf = elf
-            max_elf_i = elf_i
-        elf = 0
-        elf_i += 1
-    else:
-        elf += int(line)
-
-print(max_elf_i)
+print(max([sum([int(i) for i in s.split()]) for s in sys.stdin.read().split("\n\n")]))
